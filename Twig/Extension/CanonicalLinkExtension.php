cc4424e59dccf0cd890d436028a70382275dd845
andyexeter <palmer.andy@gmail.com>
2017-03-19 14:58:16 +0000

    Replace twig global with twig function

--- Twig/Extension/CanonicalLinkExtension.php
+++ Twig/Extension/CanonicalLinkExtension.php
@@ -1,24 +1,42 @@
 <?php
 
-
 namespace Palmtree\CanonicalUrlBundle\Twig\Extension;
 
-
 use Palmtree\CanonicalUrlBundle\Service\CanonicalUrlService;
+use Symfony\Component\HttpFoundation\RequestStack;
 
-class CanonicalLinkExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+class CanonicalLinkExtension extends \Twig_Extension
 {
     protected $canonicalUrlService;
+    protected $requestStack;
 
-    public function __construct(CanonicalUrlService $canonicalUrlService)
+    public function __construct(CanonicalUrlService $canonicalUrlService, RequestStack $requestStack)
     {
         $this->canonicalUrlService = $canonicalUrlService;
+        $this->requestStack        = $requestStack;
     }
 
-    public function getGlobals()
+    public function getFunctions()
     {
         return [
-            'palmtree_canonical_url' => $this->canonicalUrlService,
+            new \Twig_Function('palmtree_canonical_url', [$this, 'generateUrl']),
         ];
+    }
+
+    public function generateUrl($route = null, $parameters = null)
+    {
+        if (! $parameters || ! $route) {
+            $request = $this->requestStack->getCurrentRequest();
+
+            if (! $route) {
+                $route = $request->attributes->get('_route');
+            }
+
+            if (! $parameters) {
+                $parameters = $request->getQueryString();
+            }
+        }
+
+        return $this->canonicalUrlService->generateUrl($route, $parameters);
     }
 }
