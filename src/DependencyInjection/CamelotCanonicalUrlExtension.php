7d4f2550de3a84e054467c1ae89f16ba1b81a957
Gwendolen Lynch <gwendolen.lynch@gmail.com>
2022-04-19 09:11:52 +0200

    Move config directory to project root

--- src/DependencyInjection/CamelotCanonicalUrlExtension.php
+++ src/DependencyInjection/CamelotCanonicalUrlExtension.php
@@ -4,16 +4,20 @@ declare(strict_types=1);
 
 namespace Camelot\CanonicalUrlBundle\DependencyInjection;
 
+use Camelot\CanonicalUrlBundle\EventListener\ExceptionListener;
+use Camelot\CanonicalUrlBundle\EventListener\RequestListener;
+use Camelot\CanonicalUrlBundle\Routing\Generator\CanonicalUrlGenerator;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use function dirname;
 
 final class CamelotCanonicalUrlExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(dirname(__DIR__, 2) . '/config'));
         $loader->load('services.xml');
 
         $configuration = new Configuration();
